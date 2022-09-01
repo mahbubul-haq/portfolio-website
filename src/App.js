6537d12e19af658adcd40a2c4d7d961b842042f1
Mahbubul Haque <mdmahbubulhaque99@gmail.com>
2022-09-01 17:58:09 +0600

    Navbar changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Nav from './components/nav/Nav';
+import Nav from "./components/nav/Nav";
+
+import About from './components/pages/About';
+import Home from './components/pages/Home';
+import Projects from './components/pages/Projects';
+import Resume from './components/pages/Resume';
+
+
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
 
-
+  
 
   return (
     <div id="body">
       <div id="container">
-        <Nav />
-        <h2>Hello World 5</h2>
-        <h2>Hello World 5</h2>
-        <h2>Hello World 5</h2>
-        <h2>Hello World 5</h2>
-        <h2>Hello World 5</h2>
+       
+        <Router>
+          <Nav />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About/>} />
+            <Route exact path="/projects" element={<Projects/>} />
+            <Route exact path="/resume" element={<Resume/>} />
+          </Routes>
+        </Router>
+
+         
       </div>
     </div>
   );
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,26 +1,52 @@
 import React, { useEffect, useState } from "react";
+import { BsMoon, BsSun } from "react-icons/bs";
 import { FaBars } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 import "./Nav.css";
 
 const Nav = () => {
   const [isNavBarExpanded, setIsNavBarExpanded] = useState(false);
   const [resized, setResized] = useState(false);
+  const [page, setPage] = useState(0);
+  const [darkMode, setDarkMode] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const navBar = document.querySelector("#nav-container");
-    //console.log("called", window.innerWidth, isNavBarExpanded);
+    var elements = document.getElementsByClassName("nav-items");
+    for (var i = 0; i < elements.length; i++) {
+      if (i == page) {
+        elements[i].classList.add("active");
+      } else {
+        elements[i].classList.remove("active");
+      }
+    }
+  }, [page]);
+
+  useEffect(() => {
+    const navBar = document.querySelector("#nav-container-outer");
+    console.log("called", window.innerWidth, isNavBarExpanded);
     if (isNavBarExpanded) {
-      navBar.style.display = "flex";
+      navBar.style.display = "block";
     } else if (window.innerWidth <= 768) {
       navBar.style.display = "none";
     }
   }, [isNavBarExpanded, resized]);
 
   useEffect(() => {
+    if (darkMode) {
+      document.getElementById("container").classList.add("dark-mode");
+      document.getElementById("nav-container").classList.add("dark-mode");
+    } else {
+      document.getElementById("container").classList.remove("dark-mode");
+      document.getElementById("nav-container").classList.remove("dark-mode");
+    }
+  }, [darkMode]);
+
+  useEffect(() => {
     /// add event listener to window resize
     const eventListener = window.addEventListener("resize", () => {
       if (window.innerWidth > 768) {
-        document.querySelector("#nav-container").style.display = "flex";
+        document.querySelector("#nav-container-outer").style.display = "block";
         setIsNavBarExpanded(false);
         setResized(!resized);
       } else if (window.innerWidth <= 768) {
@@ -32,9 +58,29 @@ const Nav = () => {
     };
   }, []);
 
+
+  const getDarkMode = () => {
+    return (
+      <div id="dark-mode" className="dark-nav">
+          {darkMode ? <BsSun id="dark-mode-icon" onClick={
+            () => {
+              setDarkMode(!darkMode);
+            }
+          } /> : <BsMoon id="light-mode-icon" 
+          onClick={
+            () => {
+              setDarkMode(!darkMode);
+            }
+          } />}
+        </div>
+    )
+  }
+
+
   return (
     <>
       <div id="nav-icon">
+        {getDarkMode()}
         <FaBars
           id="nav-fa-bars"
           onClick={() => {
@@ -44,24 +90,50 @@ const Nav = () => {
       </div>
       <div id="nav-container-outer">
         <div id="nav-container">
-          <div className="nav-items" id="home">
+          <div
+            className="nav-items"
+            id="home"
+            onClick={() => {
+              setPage(0);
+              navigate("/");
+            }}
+          >
             Home
           </div>
-          <div className="nav-items" id="about">
+          <div
+            className="nav-items"
+            id="about"
+            onClick={() => {
+              setPage(1);
+              navigate("/about");
+            }}
+          >
             About
           </div>
-          <div className="nav-items" id="skills">
-            Skills
-          </div>
-          <div className="nav-items" id="projects">
+          <div
+            className="nav-items"
+            id="projects"
+            onClick={() => {
+              setPage(2);
+              navigate("/projects");
+            }}
+          >
             Projects
           </div>
-          <div className="nav-items" id="resume">
+          <div
+            className="nav-items"
+            id="resume"
+            onClick={() => {
+              setPage(3);
+              navigate("/resume");
+            }}
+          >
             Resume
           </div>
           <div className="nav-items" id="contact">
             Contact
           </div>
+          <div className="nav-items-dark-mode">{getDarkMode()}</div>
         </div>
       </div>
     </>
